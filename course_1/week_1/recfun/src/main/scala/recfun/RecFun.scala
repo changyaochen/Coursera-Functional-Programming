a950141770e5db05b092045c7c3db61c80248a44
Changyao Chen <changyao.chen@gmail.com>
2020-02-05 22:40:44 -0500

    finish week 1

--- course_1/week_1/recfun/src/main/scala/recfun/RecFun.scala
+++ course_1/week_1/recfun/src/main/scala/recfun/RecFun.scala
@@ -14,15 +14,34 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0) 1
+    else if (c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def runningCheck(subChars: List[Char], left: Int): Boolean = {
+      if (left < 0) false
+      else if (subChars.isEmpty) left == 0
+      else if (subChars.head == '(') runningCheck(subChars.tail, left + 1)
+      else if (subChars.head == ')') runningCheck(subChars.tail, left - 1)
+      else runningCheck(subChars.tail, left)
+    }
+
+    runningCheck(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty && money > 0) 0
+    else if (money < 0) 0
+    else if (money == 0) 1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
 }
